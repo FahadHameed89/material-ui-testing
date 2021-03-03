636ea189373b53e7a6712e11a5d3e59945fc0e2d
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2021-03-02 21:10:14 -0700

    first commit, added some random components to the landing page and installed dependencies such as core material ui, router, and icons

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Tip from './components/Tip';
+import Grid from './components/Grid';
+import RouteDisplay from './components/RouteDisplay';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Fahad Hameedd
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <Tip />
+        <Grid />
+        <RouteDisplay/>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
